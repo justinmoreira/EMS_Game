3483d228db58078cee8e3305f5beb555db1e244c
cjanua <139174067+cjanua@users.noreply.github.com>
2026-02-07 22:33:30 -0500

    Update WSL script formatting

--- scripts/wsl/new_instance.ps1
+++ scripts/wsl/new_instance.ps1
@@ -1,8 +1,10 @@
 # 1. Set Variables
 $newDistroName = "Ubuntu-Clean"
+
 $installDir = "C:\WSL\Ubuntu-Clean"
-$tarballUrl = https://cloud-images.ubuntu.com/wsl/releases/noble/current/ubuntu-noble-wsl-amd64-24.04lts.rootfs.tar.gz
+
 $tarballUrl = "https://cloud-images.ubuntu.com/wsl/noble/current/ubuntu-noble-wsl-amd64-wsl.rootfs.tar.gz"
+
 $tarballPath = "$env:TEMP\ubuntu-2404-rootfs.tar.gz"
 
 # 2. Download the RootFS (approx 70-100MB)
@@ -10,7 +12,9 @@ Write-Host "Downloading Ubuntu 24.04 RootFS..." Invoke-WebRequest -Uri $tarballU
 
 # 3. Create Directory and Import
 Write-Host "Importing into $installDir..."
+
 New-Item -ItemType Directory -Force -Path $installDir | Out-Null
+
 wsl --import $newDistroName $installDir $tarballPath
 
 # 4. Clean up
@@ -18,4 +22,5 @@ Remove-Item $tarballPath
 
 # 5. Launch
 Write-Host "Done! Launching..."
+
 wsl -d $newDistroName
