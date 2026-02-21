c93bcf8938660e783b8721180d105917c141f989
justinmoreira <58517619+justinmoreira@users.noreply.github.com>
2026-02-21 12:41:57 -0500

    [EMS-31] Calculate height/distance, create unit tests (#4)

---
+++ godot/scripts/PhysicsEngine.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# This will map to 100 pixels -> 1 km
+const PIXELS_PER_UNIT = 100.0
+
+func calculate_distance(pos1: Vector2, pos2: Vector2) -> float:
+	return pos1.distance_to(pos2) / PIXELS_PER_UNIT
+	
+func calculate_height_factor(height_tx: float, height_rx: float) -> float:
+	return 1.0 + (height_tx + height_rx) / 20.0
+	
+	
