31f09d960eff49c7fe3ffa56eb64a2e136c7cf2a
chris <chris@cjanua.xyz>
2026-02-25 20:35:08 -0500

    run ALL godot tests

---
+++ godot/scenes/tests/TestRunner.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func _ready():
+    var dir = DirAccess.open("res://scenes/tests/")
+    for file_name in dir.get_files():
+        if file_name.ends_with("Tests.gd"):
+            var script = load("res://scenes/tests/" + file_name)
+            var node = script.new()
+            add_child(node)
+
+    await get_tree().create_timer(0.5).timeout
+    get_tree().quit()
