a98c7747db9ab8b26d08be54daf2d44d5f69d6cd
chris <chris@cjanua.xyz>
2026-02-25 21:03:14 -0500

    godot editor uses tabs

--- godot/scenes/tests/TestRunner.gd
+++ godot/scenes/tests/TestRunner.gd
@@ -2,12 +2,12 @@ extends Node
 
 
 func _ready():
-    var dir = DirAccess.open("res://scenes/tests/")
-    for file_name in dir.get_files():
-        if file_name.ends_with("Tests.gd"):
-            var script = load("res://scenes/tests/" + file_name)
-            var node = script.new()
-            add_child(node)
+	var dir = DirAccess.open("res://scenes/tests/")
+	for file_name in dir.get_files():
+		if file_name.ends_with("Tests.gd"):
+			var script = load("res://scenes/tests/" + file_name)
+			var node = script.new()
+			add_child(node)
 
-    await get_tree().create_timer(0.5).timeout
-    get_tree().quit()
+	await get_tree().create_timer(0.5).timeout
+	get_tree().quit()
--- godot/scenes/ui/HUD.gd
+++ godot/scenes/ui/HUD.gd
@@ -2,11 +2,11 @@ extends CanvasLayer
 
 
 func _ready():
-    # Use the percent sign for the %Toggle unique name defined in the .tscn
-    %Toggle.toggled.connect(_on_shader_toggled)
+	# Use the percent sign for the %Toggle unique name defined in the .tscn
+	%Toggle.toggled.connect(_on_shader_toggled)
 
 
 func _on_shader_toggled(is_pressed: bool):
-    var level = get_tree().current_scene
-    if level.has_method("toggle_shader"):
-        level.toggle_shader(is_pressed)
+	var level = get_tree().current_scene
+	if level.has_method("toggle_shader"):
+		level.toggle_shader(is_pressed)
