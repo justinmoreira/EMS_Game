d164530933fa87eba82c393aa3fddd4f422f1a49
wentenglin <wenteng9@gmail.com>
2026-03-03 11:34:02 -0500

    Implement Seneor Detection

--- godot/scripts/PhysicsEngine.gd
+++ godot/scripts/PhysicsEngine.gd
@@ -10,3 +10,23 @@ func calculate_distance(pos1: Vector2, pos2: Vector2) -> float:
 
 func calculate_height_factor(height_tx: float, height_rx: float) -> float:
     return 1.0 + (height_tx + height_rx) / 20.0
+
+func calculate_distance_loss(dis: float) -> float:
+     return pow(dis+1.0, 2.0)
+
+func calculate_Srx(
+    ptx: float,
+    height_tx: float,
+    height_sensor: float,
+    distance: float,
+    terrain_loss: float =1
+) -> float:
+    var p := clampf(ptx,0.0,10.0)
+
+    var hf = calculate_height_factor(height_tx,height_sensor)
+    var dl = calculate_distance_loss(distance)
+    
+    return (p * hf) /(dl * terrain_loss)
+
+func is_detected() -> bool:
+    
