7c29c749900cd1c3051290a93b01f3fa2bd407e1
Justin <justinmoreira23@gmail.com>
2026-03-06 13:16:25 -0500

    Updated changes and unit tests

--- godot/scripts/SimulationManager.gd
+++ godot/scripts/SimulationManager.gd
@@ -1,35 +1,26 @@
 extends Node
 
-const FREQUENCY_HIGH = 2.0
-const FREQUENCY_MEDIUM = 5.0
-const FREQUENCY_LOW = 10.0
-
 var timer: Timer
 
 
 func _ready():
 	setup_timer()
 
-	# Initial State
-	set_transmission_speed(FREQUENCY_HIGH)
-
 
 func setup_timer():
 	timer = Timer.new()
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_timeout)
 	add_child(timer)
 
-	timer.one_shot = false
-	timer.timeout.connect(_on_timer_timeout)
+
+func set_transmission_speed(frequency: float) -> void:
+	var delay = remap(frequency, 30, 3000, 10.0, 0.1)
+	timer.wait_time = delay
+
+
+func send_message():
 	timer.start()
-
-
-# State Change Function
-func set_transmission_speed(seconds: float) -> float:
-	timer.wait_time = seconds
-
-	timer.start()
-
-	return seconds
 
 
 func _on_timer_timeout():
