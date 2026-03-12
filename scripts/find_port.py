24e238bdac3c2f2d9252c80de6674bdf9feb92d1
chris <chris@cjanua.xyz>
2026-03-11 21:49:43 -0400

    quick port assignment bug fix

--- scripts/find_port.py
+++ scripts/find_port.py
@@ -5,7 +5,7 @@ import socket
 for p in range(8080, 9000):
     try:
         s = socket.socket()
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 0)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("127.0.0.1", p))
         s.close()
         print(p)
