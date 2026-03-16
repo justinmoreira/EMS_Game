b87c489d0ec627463c5299ea8dc535d87676b869
cjanua <139174067+cjanua@users.noreply.github.com>
2026-03-16 17:59:48 -0400

    level1 and grid overlay (#27)

--- godot/scenes/ui/HUD.gd
+++ godot/scenes/ui/HUD.gd
@@ -2,11 +2,17 @@ extends CanvasLayer
 
 
 func _ready():
-	# Use the percent sign for the %Toggle unique name defined in the .tscn
 	%Toggle.toggled.connect(_on_shader_toggled)
+	%GridToggle.toggled.connect(_on_grid_toggled)
 
 
 func _on_shader_toggled(is_pressed: bool):
 	var level = get_tree().current_scene
 	if level.has_method("toggle_shader"):
 		level.toggle_shader(is_pressed)
+
+
+func _on_grid_toggled(is_pressed: bool):
+	var level = get_tree().current_scene
+	if level.has_method("toggle_grid"):
+		level.toggle_grid(is_pressed)
