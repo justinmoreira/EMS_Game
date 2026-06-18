0709be1fcd407afccdbae35354f93a2084fbc264
Justin <justinmoreira23@gmail.com>
2026-06-17 22:06:33 -0400

    lint

--- godot/scripts/SimulationManager.gd
+++ godot/scripts/SimulationManager.gd
@@ -258,7 +258,11 @@ func calculate_detection(srx: Unit, tx: Unit, jammers: Array) -> Dictionary:
 	)
 	var is_jammed = interference > PhysicsEngine.NOISE_FLOOR
 
-	return {"detected": is_detected.detected, "fully_detected": is_detected.fully_detected, "jammed": is_jammed}
+	return {
+		"detected": is_detected.detected,
+		"fully_detected": is_detected.fully_detected,
+		"jammed": is_jammed
+	}
 
 
 func _update_all_unit_ranges() -> void:
